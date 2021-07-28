35eb66b2eafb0287421132e35ed113bcff78f4f1
DimpleGore <80614815+DimpleGore@users.noreply.github.com>
2021-07-28 14:33:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -40,12 +40,12 @@ mongoose.connect(
   }
 );
 
-const PORT = process.env.Port || 6000;
+const port= process.env.PORT || 6000;
 
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static('client/build'))
 }
 
 app.listen(PORT, () => {
-  console.log("Server is running on port", PORT);
+  console.log("Server is running on port", port);
 });
