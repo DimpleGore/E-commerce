e46f2efb02bdfbc9a97e8c90865ea7e6f5f859c0
Dimple Gore <dimplegore2000@gmail.com>
2021-07-28 16:58:19 +0530

    update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
+//const path = require("path")
 
 
 const app = express();
@@ -48,6 +49,11 @@ const port = process.env.PORT || 5000;
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static('client/build'))
 }
+
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 
 app.listen(port, () => {
   console.log("Server is running on port", port);
